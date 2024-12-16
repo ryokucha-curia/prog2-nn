89ae69e1bb3379d0b2db42a9ac99bfa11a46aa3a
Hoshino Yuta <j3363nwv@sit.ac.jp>
2024-12-16 14:57:15 +0900

    １エポックにかかる時間を図り表示

--- train.py
+++ train.py
@@ -42,12 +42,6 @@ dataloader_test=torch.utils.data.DataLoader(
     batch_size=batch_size
 )
 
-
-for image_batch,label_batch in dataloader_test:
-    print(image_batch.shape)
-    print(label_batch.shape)
-    break
-
 #モデルのインスタンスを作成
 model=models.MyModel()
 
@@ -69,24 +63,48 @@ optimizer=torch.optim.SGD(model.parameters(),lr=learning_rate )
 acc_test=models.test_accuracy(model,dataloader_test)
 print(f'test accuracy: {acc_test*100:.2f}%')
 
-#学習
-models.train(model,dataloader_test,loss_fn,optimizer)
-
-#もう一度精度を計算
-acc_test=models.test_accuracy(model,dataloader_test)
-print(f'test accuracy: {acc_test*100:.2f}%')
-
 #学習回数
 n_epochs=5
+
+loss_train_history=[]
+loss_test_history=[]
+acc_train_history=[]
+acc_test_history=[]
 
 #学習
 for k in range(n_epochs):
     print(f'epoch{k+1}/{n_epochs}',end=': ',flush=True)
+
+    #　１　epochの学習
     loss_train=models.train(model,dataloader_test,loss_fn,optimizer)
-    print(f'train loss:{loss_train}')
+    loss_train_history.append(loss_train)
+    print(f'train loss: {loss_train:.3f}', end=', ')
+
     loss_test=models.test(model,dataloader_test,loss_fn)
-    print(f'test loss: {loss_test}')
-    acc_train=models.test_accuracy(model,dataloader_test)
-    print(f'test accuracy: {acc_train*100:.3f}%')
+    loss_test_history.append(loss_test)
+    print(f'test loss: {loss_test:.3f}', end=', ')
+
+    acc_train=models.test_accuracy(model,dataloader_train)
+    acc_train_history.append(acc_train)
+    print(f'train accuracy: {acc_train*100:.3f}%', end=', ')
+
     acc_test=models.test_accuracy(model,dataloader_test)
+    acc_test_history.append(acc_test)
     print(f'test accuracy: {acc_test*100:.3f}%')
+
+#プロット
+plt.plot(acc_train_history, label='train')
+plt.plot(acc_test_history, label='test')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(loss_train_history, label='train')
+plt.plot(loss_test_history, label='test')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.grid()
+plt.show()
