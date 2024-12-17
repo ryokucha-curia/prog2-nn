59c226c8930b66591e49f5edf7fea9d32ed91d76
Hoshino Yuta <j3363nwv@sit.ac.jp>
2024-12-17 13:26:47 +0900

    表示時間を少数点以下1桁にする

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ for k in range(n_epochs):
     loss_train=models.train(model,dataloader_test,loss_fn,optimizer)
     time_end=time.time()
     loss_train_history.append(loss_train)
-    print(f'train loss: {loss_train:.3f} ({time_end-time_start}s)', end=', ')
+    print(f'train loss: {loss_train:.3f} ({time_end-time_start:.1f}s)', end=', ')
 
     loss_test=models.test(model,dataloader_test,loss_fn)
     loss_test_history.append(loss_test)
